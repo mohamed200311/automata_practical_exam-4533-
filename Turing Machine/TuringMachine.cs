ca3d10cebe7cef55d89a0c0687a30c35a56bccdc
Mohamed Elsayed <104333591+mohamed200311@users.noreply.github.com>
2025-05-14 23:51:36 +0300

    Add files via upload

---
+++ Turing Machine/TuringMachine.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+
+namespace TMTask
+{
+    internal class TuringMachine
+    {
+        internal List<char> tape;
+        internal int HeadPosition;
+        internal string currentState;
+
+        internal TuringMachine(string StringValue)
+        {
+            tape = new List<char> { 'B' };
+            tape.AddRange(StringValue.ToCharArray());
+            tape.Add('B');
+
+            HeadPosition = 1; 
+            currentState = "Q0";
+        }
+
+        internal bool Run()
+        {
+            while (currentState != "Accept" && currentState != "Reject")
+            {
+
+                if (HeadPosition >= tape.Count) tape.Add('B');
+                else if (HeadPosition < 0)
+                {
+                    tape.Insert(0, 'B');
+                    HeadPosition = 0;
+                }
+
+                char currentSymbol = tape[HeadPosition];
+                
+
+                if(currentState == "Q0")
+                {
+                    if (currentSymbol == '0')
+                    {
+                        HeadPosition++;
+                        currentState = "Q0";
+                    }
+                    else if (currentSymbol == '1')
+                    {
+                        HeadPosition++;
+                        currentState = "Q1";
+                    }
+                    else if (currentSymbol == 'B') currentState = "Accept";
+                    else currentState = "Reject";
+                }
+                else if(currentState == "Q1")
+                {
+                    if (currentSymbol == '0')
+                    {
+                        HeadPosition++;
+                        currentState = "Q2";
+                    }
+                    else if (currentSymbol == '1')
+                    {
+                        HeadPosition++;
+                        currentState = "Q0";
+                    }
+                    else if (currentSymbol == 'B') currentState = "Reject";
+                    else  currentState = "Reject";
+                }
+                else if(currentState == "Q2")
+                {
+                    if (currentSymbol == '0')
+                    {
+                        HeadPosition++;
+                        currentState = "Q1";
+                    }
+                    else if (currentSymbol == '1')
+                    {
+                        HeadPosition++;
+                        currentState = "Q2";
+                    }
+                    else if (currentSymbol == 'B') currentState = "Reject";
+                    else  currentState = "Reject";
+                }
+            }
+
+            return currentState == "Accept";
+        }
+        internal static void RunningApp()
+        {
+            Console.WriteLine("[Turing Machine]:[Binary Numbers Divisible by 3]");
+            Console.WriteLine("---------------------------------------------");
+
+            while (true)
+            {
+                Found:
+                    Console.Write("Please Enter a Binary Number (or 'exit' to stop application): ");
+                    string BinaryString = Console.ReadLine();
+
+                if (BinaryString.ToLower() == "exit") break;
+                else if (BinaryString == "") goto Found;
+                
+                var check = new TuringMachine(BinaryString);
+                bool result = check.Run();
+                Console.WriteLine($"The number \"{BinaryString}\" is {(result ? "" : "not ")}divisible by \"3\"\n");
+            }
+        }
+    }
+}
---
+++ Turing Machine/TuringMachineTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TMTask
+{
+    [TestFixture]
+    public class TuringMachineTests
+    {
+        [Test] // Test Value = 0 
+        public void TestZero()
+        {
+            var check = new TuringMachine("0");
+            Assert.That(check.Run(),Is.True);
+        }
+
+        [Test]  // Test Value = 1 
+        public void TestOne()
+        {
+            var check = new TuringMachine("1");
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test]   // Test Value = 3
+        public void TestThree()
+        {
+            var check = new TuringMachine("11");
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test]   // Test Value = 6
+        public void TestSix()
+        {
+            var check = new TuringMachine("110");
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test]  // Test Value = 9
+        public void TestNine()
+        {
+            var check = new TuringMachine("1001");
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test] // Test Value = 10
+        public void TestTen()
+        {
+            var check = new TuringMachine("1010");
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test]   // Test Value = 15
+        public void TestFifteen()
+        {
+            var check = new TuringMachine("1111");
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test] // Test Value = 21 
+        public void TestTwentyOne()
+        {
+            var check = new TuringMachine("10101");
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test] // Test Value = Empty
+        public void TestEmptyString()
+        {
+            var check = new TuringMachine("");
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test] // Test Value = 54
+        public void TestLargeNumber()
+        {
+            var check = new TuringMachine("110110"); 
+            Assert.That(check.Run(), Is.True);
+        }
+
+        [Test] // Test Value contain Digits 
+        public void TestInvalidInput()
+        {
+            var check = new TuringMachine("1021");
+            Assert.That(check.Run(), Is.True);
+        }
+        [Test] // Test Value contain Characters  
+        public void TestInvalidInputString()
+        {
+            var check = new TuringMachine("mohamed");
+            Assert.That(check.Run(), Is.True);
+        }
+    }
+}
